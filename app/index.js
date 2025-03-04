f4460148b943ba456fdbce3f4806493b8c397828
aleksey-t <megapozitivizm@yandex.ru>
2025-03-04 11:16:46 +0300

    add cors

--- app/index.js
+++ app/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 app.use("/api/ingredients", require("./controllers/ingredientsController"));
 app.use("/api/recipes", require("./controllers/recipeController"));
 app.use("/api/users", require("./controllers/userController"));
